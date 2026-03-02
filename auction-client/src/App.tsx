78071344bcc61c3ac1943b0d2cc413a19db02bd1
Onehundra <hermanssonkevin@gmail.com>
2026-03-02 22:58:07 +0100

    klar

--- auction-client/src/App.tsx
+++ auction-client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import {  Routes, Route, useLocation } from "react-router-dom"
 
 import { getOpenAuctions } from "./services/AuctionService"
 import type { Auction } from "./types/Types"
